d1bf9b090d9a4bbdb3ba377a7a218f269475905d
mirrorming <mirrorming@qq.com>
2020-10-28 00:07:44 +0800

    init project😁😁😁

---
+++ awesome-todo-server/src/main/java/io/github/mirrormingzz/todo/AwesomeTodoApplication.java
@@ -0,0 +1,13 @@
+package io.github.mirrormingzz.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomeTodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwesomeTodoApplication.class, args);
+    }
+
+}
---
+++ awesome-todo-server/src/test/java/io/github/mirrormingzz/todo/AwesomeTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mirrormingzz.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwesomeTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
